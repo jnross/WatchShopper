74b3c3524cac74717e9d75a9675752a4e3ae6134
Joseph Ross <jnross@gmail.com>
2014-01-07 23:44:32 -0800

    Initial commit with JS file.  Works with dummy data.

---
+++ watch_shopper/src/js/pebble-js-app.js
@@ -0,0 +1,55 @@
+Pebble.addEventListener("ready",
+    function(e) {
+        console.log("Hello world! - Sent from your javascript application.");
+        sendChecklist(fake_checklist);
+    }
+);
+
+var fake_checklist = {};
+fake_checklist.name = "A Fake Checklist";
+fake_checklist.list_id = 0;
+var fake_item = {};
+fake_item.name = "A Fake Item";
+fake_item.flags = 0;
+fake_item.item_id = 0;
+fake_checklist.items = [fake_item];
+
+function sendChecklist(checklist) {
+	var data = buildDataForChecklist(checklist);
+	console.log("data to send: " + data);
+  	var transactionId = Pebble.sendAppMessage( { "CMD_LIST_ITEMS_START": data },
+		function(e) {
+		console.log("Successfully delivered message with transactionId="
+		  + e.data.transactionId);
+		},
+		function(e) {
+		console.log("Unable to deliver message with transactionId="
+		  + e.data.transactionId
+		  + " Error is: " + e.error.message);
+		}
+  	);
+}
+
+function buildDataForChecklist(checklist) {
+	var data = [];
+	data.push(checklist.list_id);
+	data.push(checklist.name, 0);
+	data.push(checklist.items.length);
+	console.log("items count: " + checklist.items.length);
+	for (var i = 0; i < checklist.items.length; i++) {
+
+		var item = checklist.items[i];
+		var itemData = buildDataForChecklistItem(item);
+		console.log("itemData: " + itemData);
+		data = data.concat(itemData);
+	}
+	return data;
+}
+
+function buildDataForChecklistItem(item) {
+	var data = [];
+	data.push(item.item_id);
+	data.push(item.name, 0);
+	data.push(item.flags);
+	return data;
+}
