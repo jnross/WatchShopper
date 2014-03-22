fd5b5bfb69a7bce1f9e461a2af27b683dfa425dc
Joseph Ross <jnross@gmail.com>
2014-03-22 10:30:05 -0700

    Send checked items to the bottom of the list so you can see what's left.

--- watch_shopper/src/items_window.c
+++ watch_shopper/src/items_window.c
@@ -6,15 +6,45 @@
 static MenuLayer *items_menu;
 static Window *items_window;
 
-#define NUM_MENU_SECTIONS 1
+#define NUM_MENU_SECTIONS 2
+#define SECTION_UNCHECKED 0
+#define SECTION_CHECKED 1
 
 static List *checklist;
 
+static ListItem **unchecked_items = NULL;
+static int unchecked_items_count = 0;
+static ListItem **checked_items = NULL;
+static int checked_items_count = 0;
+
 static void discard_checklist();
+
+static void refresh_checked_items() {
+  if (checklist == NULL) { return; }
+  if (checked_items == NULL) {
+    checked_items = malloc(sizeof(ListItem *) * checklist->count);
+  }
+  if (unchecked_items == NULL) {
+    unchecked_items = malloc(sizeof(ListItem *) * checklist->count);
+  }
+
+  unchecked_items_count = 0;
+  checked_items_count = 0;
+
+  for (int i = 0; i < checklist->count; i++) {
+    ListItem *item = checklist->items[i];
+    if (item->isChecked) {
+      checked_items[checked_items_count++] = item;
+    } else {
+      unchecked_items[unchecked_items_count++] = item;
+    }
+  }
+}
 
 void reload_if_necessary() {
  	// Don't reload the menu until we've loaded all the items.  If we do, the app will crash when attempting to access bad memory.
  	if (checklist != NULL && checklist->count == checklist->expected_count) {
+      refresh_checked_items();
     	menu_layer_reload_data(items_menu);
  	}
 }
@@ -87,14 +117,24 @@ static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t secti
 	if (checklist == NULL) {
 		return 0;
 	} else {
-  		return checklist->count;
+    if (section_index == SECTION_UNCHECKED) {
+  		return unchecked_items_count;
+    } else if (section_index == SECTION_CHECKED) {
+      return checked_items_count;
+    } else {
+      return 0;
+    }
 	}
 }
 
 // A callback is used to specify the height of the section header
 static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
   // This is a define provided in pebble_os.h that you may use for the default height
-  return MENU_CELL_BASIC_HEADER_HEIGHT;
+  if (section_index == 0) {
+    return MENU_CELL_BASIC_HEADER_HEIGHT;
+  } else {
+    return 0;
+  }
 }
 
 // A callback is used to specify the height of the section header
@@ -105,13 +145,21 @@ static int16_t menu_get_cell_height_callback(MenuLayer *menu_layer, MenuIndex *c
 
 // Here we draw what each header is
 static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
-  menu_cell_basic_header_draw(ctx, cell_layer, checklist->name);
+  if (section_index == 0) {
+    menu_cell_basic_header_draw(ctx, cell_layer, checklist->name);
+  }
 }
 
 // This is the menu item draw callback where you specify what each item should look like
 static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
-  if (cell_index->row < checklist->count) {
-    ListItem *item = checklist->items[cell_index->row];
+  ListItem *item = NULL;
+  if (cell_index->section == SECTION_UNCHECKED) {
+    item = unchecked_items[cell_index->row];
+  } else if (cell_index->section == SECTION_CHECKED) {
+    item = checked_items[cell_index->row];
+  }
+
+  if (item != NULL) {
     char* item_name = item->name;
 
     GFont font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
@@ -138,18 +186,43 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
 
 // Here we capture when a user selects a menu item
 void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
-  ListItem *item = checklist->items[cell_index->row];
-  item->isChecked = !(item->isChecked);
-  send_check_item(checklist->list_id, item->item_id, item->isChecked);
-  menu_layer_reload_data(menu_layer);
+  ListItem *item = NULL;
+  if (cell_index->section == SECTION_UNCHECKED) {
+    item = unchecked_items[cell_index->row];
+  } else if (cell_index->section == SECTION_CHECKED) {
+    item = checked_items[cell_index->row];
+  }
+  // Draw a line through newly-checked items.
+  if (item->isChecked) {
+    graphics_context_set_fill_color(ctx, GColorBlack);
+    GRect line_rect;
+    line_rect.origin = (GPoint) {.x = 0, .y = (rect.size.h / 2) + 0};
+    line_rect.size = (GSize) {.w = rect.size.w, .h = 2};
+    graphics_fill_rect(ctx, line_rect, 0, GCornerNone);
+  }
+
+  if (item != NULL) {
+    item->isChecked = !(item->isChecked);
+    send_check_item(checklist->list_id, item->item_id, item->isChecked);
+    refresh_checked_items();
+    menu_layer_reload_data(menu_layer);
+  }
 
 }
 
 static void discard_checklist() {
-    if (checklist != NULL) {
-    	list_destroy(checklist);
-    	checklist = NULL;
+  if (checklist != NULL) {
+    list_destroy(checklist);
+    checklist = NULL;
 	}
+  if (checked_items != NULL) {
+    free(checked_items);
+    checked_items = NULL;
+  }
+  if (unchecked_items != NULL) {
+    free(unchecked_items);
+    unchecked_items = NULL;
+  }
 }
 
 static void items_window_load(Window *window) {
@@ -177,6 +250,7 @@ static void items_window_load(Window *window) {
 
   // Add it to the window for display
   layer_add_child(window_layer, menu_layer_get_layer(items_menu));
+  refresh_checked_items();
   menu_layer_reload_data(items_menu);
 
 }
